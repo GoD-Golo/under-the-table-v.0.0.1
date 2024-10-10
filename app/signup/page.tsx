0dcf61fd87b8dba4961c319c1d87b36c9de0d00f
Gologan Alexandru <dragosgologan17@gmail.com>
2024-10-10 12:11:26 +0300

    solved issues for task: 06, 03

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,11 +1,6 @@
-"use client";
-import { useState } from "react";
-import { Input } from "@nextui-org/input";
-import { Button } from "@nextui-org/react";
-
 import Navbar from "@/components/Navbar";
-import { NavbarButtons } from "@/types/navbar";
-import { supabase } from "@/lib/supabase";
+import { NavbarButtons } from "@/utils/types";
+import StyledInput from "@/components/ui/StyledInput";
 
 const buttons: NavbarButtons[] = [
   {
@@ -16,47 +11,6 @@ const buttons: NavbarButtons[] = [
 ];
 
 export default function SignUp() {
-  const [credentials, setCredentials] = useState<{
-    email: string;
-    password: string;
-  }>({ email: "", password: "" });
-
-  const [error, setError] = useState<string | null>(null);
-  const [success, setSuccess] = useState<string | null>(null);
-
-  const signup = async () => {
-    try {
-      let { data, error } = await supabase.auth.signUp({
-        email: credentials.email,
-        password: credentials.password,
-      });
-
-      if (error) {
-        setError(error.message);
-        setSuccess(null);
-      }
-
-      if (data.user) {
-        setSuccess(
-          "Sign Up successful. Please check your email for verification link."
-        );
-        setError(null);
-      } else {
-        setError("Unexpected error. Please try again.");
-        setSuccess(null);
-      }
-      console.log(data);
-    } catch (error) {
-      setError("Unexpected error. Please try again.");
-    }
-  };
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    setCredentials({ ...credentials, [e.target.id]: e.target.value });
-    console.log(credentials);
-  };
-
   return (
     <div className="utt-dark">
       <Navbar buttons={buttons} />
@@ -64,33 +18,19 @@ export default function SignUp() {
         <div className="flex flex-col items-center justify-center min-h-screen">
           <h1 className="text-4xl font-bold text-amber-500 pb-10">Sign Up</h1>
           <form className="flex flex-col space-y-4 max-w-lg">
-            <Input
-              classNames={{
-                inputWrapper: "bg-gray-800",
-                innerWrapper: "bg-gray-800",
-              }}
-              color="primary"
-              type="email"
-              id="email"
-              label="Email"
-              value={`${credentials.email}`}
+            <StyledInput
+              type="text"
+              label="Username"
+              id="username"
               width="w-72"
-              variant="faded"
-              onChange={handleChange}
             />
-            <Input
-              classNames={{
-                inputWrapper: "bg-gray-800",
-                innerWrapper: "bg-gray-800",
-              }}
-              color="primary"
+            <StyledInput type="email" label="Email" id="email" width="w-72" />
+            <StyledInput
               type="password"
               id="password"
               label="Password"
-              value={`${credentials.password}`}
-              variant="faded"
+              id="password"
               width="w-72"
-              onChange={handleChange}
             />
 
             <div className="w-72 pt-10">
@@ -102,8 +42,6 @@ export default function SignUp() {
               </Button>
             </div>
           </form>
-          {success && <p className="text-emerald-400 mt-4">{success}</p>}
-          {error && <p className="text-rose-600 mt-4">{error}</p>}
         </div>
       </div>
     </div>
--- types/navbar.tsx
+++
@@ -1,9 +0,0 @@
-export interface NavbarButtons {
-  text: string;
-  link: string;
-  type: "primary" | "secondary";
-}
-
-export interface NavbarButtonsList {
-  buttons: NavbarButtons[];
-}
